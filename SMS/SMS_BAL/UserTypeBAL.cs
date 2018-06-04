2e17d8779eb1570959578d30d41a49826201222c
tscp208 <39762792+tscp208@users.noreply.github.com>
2018-06-04 17:11:25 +0530

    New Version

---
+++ SMS/SMS_BAL/UserTypeBAL.cs
@@ -0,0 +1,49 @@
+﻿using SMS_DAL;
+using SMS_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_BAL
+{
+    public class UserTypeBAL
+    {
+        public UserTypeDAL userTypeDAL = new UserTypeDAL();
+
+        public List<UserTypeEntity> GetUserTypes()
+        {
+            return userTypeDAL.GetUserTypes();
+        }
+
+        public UserTypeEntity GetUserTypeByID(int userTypeID)
+        {
+            return userTypeDAL.GetUserTypeByID(userTypeID);
+        }
+
+        public bool UpdateUserType(UserTypeEntity userType)
+        {
+            if(userType.UserTypeID > 0)
+            {
+                //Update
+                return userTypeDAL.UpdateUserType(userType);
+            }
+            else
+            {
+                //Insert
+                return userTypeDAL.InsertUserType(userType);
+            }
+        }
+
+        public bool IsUserTypeExist(string userType, int userTypeID)
+        {
+            return userTypeDAL.IsUserTypeExist(userType, userTypeID);
+        }
+
+        public bool DeleteUserType(int userType)
+        {
+            return userTypeDAL.DeleteUserType(userType);
+        }
+    }
+}
